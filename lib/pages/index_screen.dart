938918651fa9bcdf88034869f7cde4eb9d7f8ad6
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-11 19:55:08 +0600

    11.9

--- lib/pages/index_screen.dart
+++ lib/pages/index_screen.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class IndexScreen extends StatelessWidget {
   const IndexScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
-      body: Center(
-        child: Text(
-          'This is index Screen',
-          style: TextStyle(
-              fontFamily: "Poppins", fontWeight: FontWeight.bold, fontSize: 22),
-        ),
-      ),
-    ));
+            body: Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('images/bg.png'))),
+      child: ListView(children: [
+        Padding(
+          padding:
+              const EdgeInsets.only(top: 35, bottom: 30, left: 16, right: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: Text("LEARN QURAN",
+                    style: TextStyle(
+                        fontSize: 20.w,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700)),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: const Color(0xffE5E5E5),
+                      onPrimary: Colors.blue,
+                      padding: const EdgeInsets.only(
+                          top: 16, bottom: 16, left: 10, right: 10)),
+                  onPressed: () {},
+                  child: const Text(
+                    "আরবি ভাষার বর্ণ",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ))
+            ],
+          ),
+        )
+      ]),
+    )));
   }
 }
