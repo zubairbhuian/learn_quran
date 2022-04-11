35260224deb19f8b924ed620db3d3a435bc8d95b
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-11 09:37:05 +0600

    11.1

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-
-import 'index_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -12,23 +9,60 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    Timer(
-        const Duration(seconds: 4),
-        (() => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (_) => const IndexScreen()))));
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   Timer(
+  //       const Duration(seconds: 3),
+  //       (() => Navigator.pushReplacement(
+  //           context, MaterialPageRoute(builder: (_) => const IndexScreen()))));
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover, image: AssetImage("images/bg.png")))),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover, image: AssetImage("images/bg.png"))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 200.h,
+              ),
+              Container(
+                height: 230,
+                width: 250,
+                color: const Color.fromARGB(0, 254, 254, 254),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 166,
+                      width: 166,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("images/quran.png"))),
+                    ),
+                    const SizedBox(
+                      height: 13,
+                    ),
+                    const Text(
+                      'LEARN QURAN',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+              
+            ],
+          ),
+        ),
       ),
     );
   }
