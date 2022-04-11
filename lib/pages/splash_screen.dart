e311611f8a02a904609836ccd824ac800c1d069d
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-11 18:53:54 +0600

    11.8

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -64,43 +64,42 @@ class _SplashScreenState extends State<SplashScreen> {
               Flexible(
                 fit: FlexFit.tight,
                 flex: 1,
-                child: Container(
-                  child:
-                      Stack(alignment: AlignmentDirectional.center, children: [
-                    Positioned(
-                        bottom: 120,
-                        child: Text("Powered by",
-                            style: TextStyle(
-                                fontSize: 18.w, color: Colors.white))),
-                    Positioned(
-                      bottom: 60,
-                      child: Container(
-                        padding: EdgeInsets.all(9.w),
-                        height: 50,
-                        width: 230,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: Colors.white, width: 1)),
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 40.h,
-                              width: 40.w,
-                              decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage('images/logo.png'))),
-                            ),
-                            Text(
-                              "MASTER-ACADEMY",
-                              style: TextStyle(
-                                  fontSize: 15.w, color: Colors.white),
-                            )
-                          ],
-                        ),
+                child: Stack(alignment: AlignmentDirectional.center, children: [
+                  Positioned(
+                      bottom: 120,
+                      child: Text("Powered by",
+                          style: TextStyle(
+                              fontSize: 18.w,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300))),
+                  Positioned(
+                    bottom: 60,
+                    child: Container(
+                      padding: EdgeInsets.all(9.w),
+                      height: 50,
+                      width: 230,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.white, width: 1)),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40.h,
+                            width: 40.w,
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('images/logo.png'))),
+                          ),
+                          Text(
+                            "MASTER-ACADEMY",
+                            style:
+                                TextStyle(fontSize: 15.w, color: Colors.white),
+                          )
+                        ],
                       ),
                     ),
-                  ]),
-                ),
+                  ),
+                ]),
               )
             ],
           ),
