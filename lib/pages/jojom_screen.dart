d00e7f3be6892066e721115f6ae8d3da5ac9aabd
“[Zubair <[zubairbhuian@gmail.com]>
2022-04-24 12:16:21 +0600

    24.6

--- lib/pages/jojom_screen.dart
+++ lib/pages/jojom_screen.dart
@@ -28,8 +28,90 @@ class JojomScreen extends StatelessWidget {
                     padding: EdgeInsets.only(
                         top: 35, bottom: 30, left: 20.h, right: 20.h),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: const [JojomText()],
+                      // crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const JojomText(),
+                        Container(
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(width: 2, color: Colors.white)),
+                          child: Column(children: [
+                            Container(
+                              color: AppsColor.skyBlue,
+                              child: Row(
+                                children: [
+                                  Flexible(
+                                    flex: 2,
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                          color: AppsColor.skyBlue,
+                                          border: Border(
+                                              right: BorderSide(
+                                                  width: 2,
+                                                  color: Colors.white))),
+                                      height: 50,
+                                      child: const Center(
+                                        child: Text(
+                                          '1',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Flexible(
+                                    flex: 5,
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                          color: AppsColor.skyBlue,
+                                          border: Border(
+                                              right: BorderSide(
+                                                  width: 2,
+                                                  color: Colors.white))),
+                                      height: 50,
+                                    ),
+                                  ),
+                                  Flexible(
+                                    flex: 2,
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                          color: AppsColor.skyBlue,
+                                          border: Border(
+                                              right: BorderSide(
+                                                  width: 2,
+                                                  color: Colors.white))),
+                                      height: 50,
+                                      child: const Center(
+                                        child: Text(
+                                          '2',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Flexible(
+                                    flex: 5,
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                          color: AppsColor.skyBlue,
+                                          border: Border(
+                                              right: BorderSide(
+                                                  width: 0,
+                                                  color: Colors.white))),
+                                      height: 50,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ]),
+                        )
+                      ],
                     ),
                   )
                 ]),
@@ -51,8 +133,8 @@ class JojomText extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 29,
-            width: 100,
+            height: 22,
+            width: 70,
             decoration: const BoxDecoration(
                 color: AppsColor.lightYellow,
                 image: DecorationImage(image: AssetImage('images/jojom.png'))),
